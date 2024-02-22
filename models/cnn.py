1318b8196de3b1250e2db0489d87d19c3401ac1f
Sunghyun Cho <hey@cho.sh>
2024-02-22 02:03:51 -0800

    Refactor CNN model and add new TripleConvPoolReLU_FlatFC_CNN model

--- models/cnn.py
+++ models/cnn.py
@@ -1,14 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
 import torchvision.transforms as transforms
 
 
-class CNN(nn.Module):
+class DualConvPair_MaxPool_DualFCReLU_CNN(nn.Module):
     def __init__(self, num_classes):
-        super(CNN, self).__init__()
+        super(DualConvPair_MaxPool_DualFCReLU_CNN, self).__init__()
         self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
         self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
         self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -35,4 +33,33 @@ class CNN(nn.Module):
         out = self.fc1(out)
         out = self.relu1(out)
         out = self.fc2(out)
+        return out
+
+
+class TripleConvPoolReLU_FlatFC_CNN(nn.Module):
+    def __init__(self, num_classes):
+        super(TripleConvPoolReLU_FlatFC_CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc = nn.Linear(128 * 4 * 4, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
         return out
